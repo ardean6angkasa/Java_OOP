b1fd923a3c7d6cbc50c23c8ede5515a65642aaad
angkasa <rezaangkasa@outlook.com>
2023-11-09 18:55:13 +0700

    java

--- abstract_method_void.java
+++ abstract_method_void.java
@@ -1,9 +1,9 @@
-abstract class ppt8{
+abstract class abstract_method_void{
     public String fname="john";
     public int age=24;
     public abstract void study();
 }
-class Student extends ppt8{
+class Student extends abstract_method_void{
     public int graduationYear=2018;
     public void study(){
         System.out.println("Studying all day along");
